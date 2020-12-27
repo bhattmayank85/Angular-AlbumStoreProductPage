652592d909c3b25f717f6f8be59f32a8f87fae40
Mayank <bhattmayank85@gmail.com>
2020-12-27 02:43:47 -0500

    Fix to run the application

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
-import { map } from 'rxjs/add/operator/map';
+import { map } from 'rxjs/operators';
 import {Album} from './album';
 import {Observable} from 'rxjs/Observable';
 import {Product} from './product';
@@ -14,11 +14,11 @@ export class ProductService {
   constructor(private _http: Http) {}
 
   getAlbum(id: number) : Observable<Album> {
-    return this._http.get(this._albumUrl).map(response => <Album>response.json());
+    return this._http.get(this._albumUrl).pipe(map(response => <Album>response.json()));
   }
 
   getProducts(): Observable<Product[]> {
-    return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
+    return this._http.get(this._productsUrl).pipe(map(response => <Product[]>response.json()));
   }
 
 }
